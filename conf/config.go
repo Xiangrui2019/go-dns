c22f720c4746b164c9fa5e8f131070e41ed8b67a
Xiangrui2019 <xiangrui@aiursoft.com>
2019-07-09 19:54:25 +0800

    dsafs

---
+++ conf/config.go
@@ -0,0 +1,23 @@
+package conf
+
+package conf
+
+import "github.com/BurntSushi/toml"
+
+type Config struct {
+	Host          string `toml:"HOST"`
+	Port          int    `toml:"PORT"`
+	MySQLDSN      string `toml:"MYSQL_DSN"`
+}
+
+func LoadConfigFromFile(filepath string) (*Config, error) {
+	var conf Config
+
+	_, err := toml.DecodeFile(filepath, &conf)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &conf, nil
+}
